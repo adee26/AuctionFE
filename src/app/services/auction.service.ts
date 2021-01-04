77af8c3d0383bdc8f9dd204515feb47fbc6f8866
Adelina <apetreiadelina@yahoo.com>
2021-01-04 21:54:29 +0200

    Created forward proxy and auction service.

---
+++ src/app/services/auction.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+
+@Injectable()
+export class AuctionService {
+
+  constructor(private http: HttpClient) { }
+
+  getAuctions(){
+    return this.http.get('/server/api/v1/auctions');
+  }
+}
