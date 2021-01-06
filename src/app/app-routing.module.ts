d346386382664b9d09633d027bfc52e55f3dcf33
Adelina <apetreiadelina@yahoo.com>
2021-01-06 22:14:35 +0200

    Created auction component. Linked it to the auction service. Created html page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import {RegisterComponent} from './register/register.component';
 import {LoginComponent} from './login/login.component';
 import {AppComponent} from './app.component';
+import {AuctionComponent} from './components/auction/auction.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/', pathMatch: 'full' },
   { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent }];
+  { path: 'login', component: LoginComponent },
+  { path: 'auction', component: AuctionComponent},
+  { path: 'auction/:id', component: AuctionComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
